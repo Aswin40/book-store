0d14469bc452e50f1a9e2c65d1a44d700ceb18c1
Aswin-Pradeep-P <aswin9394@gmail.com>
2024-12-05 09:57:57 +0530

    add validations for book entities

--- src/modules/book/createBook.dto.ts
+++ src/modules/book/createBook.dto.ts
@@ -1,7 +1,28 @@
-export default interface CreateBookDto {
-  name: string;
-  price: string;
+import { IsDefined, IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
+export default class CreateBookDto {
+  @IsNotEmpty()
+  @IsDefined()
+  @IsString()
+  public name: string;
+
+  @IsNotEmpty()
+  @IsDefined()
+  @IsString()
+  public price: string;
+
+  @IsNotEmpty()
+  @IsDefined()
+  @IsNumber()
   authorId: number;
+
+  @IsNotEmpty()
+  @IsDefined()
+  @IsNumber()
   categoryId: number;
+
+  @IsNotEmpty()
+  @IsDefined()
+  @IsNumber()
   pulisherId: number;
 }
--- src/modules/book/updateBook.dto.ts
+++ src/modules/book/updateBook.dto.ts
@@ -1,5 +1,18 @@
-export interface UpdateBookDto {
+import { IsDefined, IsNotEmpty, IsString } from 'class-validator';
+
+export class UpdateBookDto {
+  @IsNotEmpty()
+  @IsDefined()
+  @IsString()
   id: string;
+
+  @IsNotEmpty()
+  @IsDefined()
+  @IsString()
   name: string;
+
+  @IsNotEmpty()
+  @IsDefined()
+  @IsString()
   price: string;
 }
